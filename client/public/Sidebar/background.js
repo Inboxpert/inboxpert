7f1378692bbaf5c4d30ae69d67e8f876b214a34b
adit-ghosh <aditghosh5112005@gmail.com>
2025-08-09 14:49:56 +0530

    Deleted Frontend Restarting it

--- client/public/Sidebar/background.js
+++
@@ -1,9 +0,0 @@
-chrome.runtime.onInstalled.addListener(() => {
-  console.log("Gmail Sorter extension installed.");
-});
-
-function checkSecureUrl(url) {
-  if (!url.startsWith('https://')) {
-    throw new Error('Insecure connection: ' + url);
-  }
-}
