5df2de06e7011050cdc0914790d10ea15cac034e
adit-ghosh <aditghosh5112005@gmail.com>
2025-08-09 15:04:16 +0530

    Configure Tailwind

---
+++ client/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'ai-primary': '#4F46E5',
+        'ai-secondary': '#10B981',
+      },
+    },
+  },
+  plugins: [],
+}
